36804ca039a928527f29caaa38c763d9e39b6421
Khaled El Samman <k.elsamman@student.han.nl>
2025-10-22 12:13:32 +0200

    Update HANLinkedLIst.java

--- startcode/src/main/java/nl/han/ica/datastructures/HANLinkedLIst.java
+++ startcode/src/main/java/nl/han/ica/datastructures/HANLinkedLIst.java
@@ -1,25 +1,36 @@
 package nl.han.ica.datastructures;
 
+import javax.swing.tree.TreeNode;
+
 public class HANLinkedLIst implements IHANLinkedList<T> {
+
+    private Node<T> head;
+
 
     @Override
     public void addFirst(T value) {
-
+        if (head == null){
+            head = new Node<>((T) value);
+        } else {
+            Node<T> newTreeNode = new Node();
+            newTreeNode.value = value;
+            head = newTreeNode;
+        }
     }
 
     @Override
     public void clear() {
-
+head = null;
     }
 
     @Override
     public void insert(int index, T value) {
-
+Node<T> addNode = new Node<T>();
+addNode.value = value;
     }
 
     @Override
     public void delete(int pos) {
-
     }
 
     @Override
