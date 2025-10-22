4bd86df94fec303a7ac247d55dfa128ea3865fdd
Khaled El Samman <k.elsamman@student.han.nl>
2025-10-22 11:21:28 +0200

    hanStack

--- startcode/src/main/java/nl/han/ica/datastructures/HANStack.java
+++ startcode/src/main/java/nl/han/ica/datastructures/HANStack.java
@@ -2,18 +2,32 @@ package nl.han.ica.datastructures;
 
 public class HANStack implements IHANStack<T> {
 
+    private Node<T> head;
+
     @Override
     public void push(T value) {
+        Node T  newNode = new Node<T> value;
+        newNode.setNext(head);
+        head = newNode;
 
     }
 
     @Override
     public T pop() {
-        return null;
+        if (head == null){
+            throw new IllegalStateException("Stack Empty");
+        }
+        Node <T> previousHead = head;
+        head = head.getNext();
+        return (T) previousHead.getValue();
     }
 
     @Override
     public T peek() {
+        if (head == null){
         return null;
-    }
+    } else {
+            return (T) head.getValue();
+        }
+
 }
---
+++ startcode/src/main/java/nl/han/ica/datastructures/Node.java
@@ -0,0 +1,23 @@
+package nl.han.ica.datastructures;
+
+public class Node<T> {
+    private Node <T> next;
+    private T value = null;
+
+    public Node(T value) {
+        this.value = value;
+    }
+public void setNext(Node<T> next){
+this.next = next;
+
+}
+
+public Node<T> getNext(){
+        return next;
+}
+public T getValue(){
+        return value;
+}
+
+
+}
